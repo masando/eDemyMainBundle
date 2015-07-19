07cbccd51315a673b02948c6da366b60fa372f77
Manuel Sanchís <msanchis@edemy.es>
2015-07-19 16:37:33 +0200

    not found exception and redirect

--- Controller/RedirectController.php
+++ Controller/RedirectController.php
@@ -3,11 +3,16 @@
 namespace eDemy\MainBundle\Controller;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use eDemy\MainBundle\Event\ContentEvent;
 use eDemy\MainBundle\Entity\Param;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use eDemy\MainBundle\Entity\Notfound;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Symfony\Component\Security\Core\Exception\AuthenticationCredentialsNotFoundException;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 
 class RedirectController extends BaseController
 {
@@ -37,23 +42,59 @@ class RedirectController extends BaseController
 
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
+        $redirect = null;
         $exception = $event->getException();
-
         if ($exception instanceof HttpExceptionInterface) {
             if($exception->getStatusCode() == '404') {
+                // SAVE URI IN DATABASE
                 $entity = $this->saveUrl();
-                //die(var_dump((array) json_decode($entity->getOptions())));
-                if($entity) {
-                    $options = (array) json_decode($entity->getOptions());
-                    $options = array_merge($options, array('_locale' => 'es'));
-                    //die(var_dump($this->get('router')->generate($entity->getRedirect(), $options)));
-                    $response = new RedirectResponse($this->get('router')->generate($entity->getRedirect(), $options), 302);
-                } else {
-                    $response = new RedirectResponse($this->get('router')->generate('edemy_main_frontpage'), 302);
+                // IF ENTITY HAS REDIRECT FOLLOW
+                $options = (array) json_decode($entity->getOptions());
+                $options = array_merge($options, array('_locale' => 'es'));
+                try {
+                    $entityRedirect = $entity->getRedirect();
+                    $redirect = $this->get('router')->generate($entityRedirect, $options);
+                    $response = new RedirectResponse($redirect, 302);
+                    $event->setResponse($response);
+                    $event->stopPropagation();
+                } catch (RouteNotFoundException $e) {
+                    if($entityRedirect) {
+                        $url = $entityRedirect;
+                    } else {
+                        // IF REDIRECT IS NO ROUTE
+                        if(($rule = $this->getParam('redirect.rule')) !== 'redirect.rule') {
+                            $url = $this->getRequest()->getRequestUri();
+                            if($url[0] == '/') {
+                                $url = substr($url, 1);
+                            }
+                            $url = str_replace('{url}', $url, $rule);
+                        }
+                    }
+
+                    $response = new RedirectResponse($url, 302);
+                    $event->setResponse($response);
+                    $event->stopPropagation();
+
+                    return true;
                 }
-                //die(var_dump($response));
-                $event->setResponse($response);
-                $event->stopPropagation();
+                // IF NOT REDIRECT GENERATE 404
+                $contentEvent = new ContentEvent('edemy_notfound');
+                $contentEvent->setFormat('html');
+                $this->addEventModule($contentEvent, 'templates/main/redirect/notfound');
+                if($content = $this->getContent($contentEvent->getRoute())) {
+                    $contentEvent->setContent($content);
+                }
+                $token = new AnonymousToken('main', 'anon.');
+                $this->get('security.token_storage')->setToken($token);
+                $this->get('request')->getSession()->invalidate();
+                try {
+                    if ($response = $this->getFullResponse($contentEvent)) {
+
+                        $event->setResponse($response);
+                    }
+                } catch(AuthenticationCredentialsNotFoundException $ae) {
+                    //die(var_dump($contentEvent));
+                }
             }
             //$response->setStatusCode($exception->getStatusCode());
             //$response->headers->replace($exception->getHeaders());
@@ -67,13 +108,12 @@ class RedirectController extends BaseController
         $currentUrl = $this->getRequest()->getUri();
         $entity = $em->getRepository('eDemyMainBundle:Notfound')->findOneByUrl($currentUrl);
         if(!$entity) {
-            $notfound = new Notfound($em);
-            $notfound->setUrl($currentUrl);
-            $em->persist($notfound);
+            $entity = new Notfound($em);
+            $entity->setUrl($currentUrl);
+            $em->persist($entity);
             $em->flush();
-            
-            return false;
         }
+
         return $entity;
     }
 }
