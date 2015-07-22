3841cfd8d6fc896d708237a7ebee875b9fc1fb5f
Manuel Sanchís <msanchis@edemy.es>
2015-07-22 16:58:55 +0200

    tags with auto color

--- Twig/PathExtension.php
+++ Twig/PathExtension.php
@@ -53,8 +53,34 @@ class PathExtension extends \Twig_Extension
     {
         return array(
             new \Twig_SimpleFunction('path', array($this, 'pathFunction')),
+            new \Twig_SimpleFunction('stringToColor', array($this, 'stringToColorFunction')),
             new \Twig_SimpleFunction('cdnjs', array($this, 'jqueryFunction'), array('is_safe' => array('html'), 'pre_escape' => 'html')),
         );
+    }
+
+    public function stringToColorFunction($str, $opacity = 0.5) {
+        $code = dechex(crc32($str));
+        $code = substr($code, 0, 6);
+        $rgb = $this->hex2rgb($code);
+
+        return "background-color: rgba(". $rgb[0] . "," . $rgb[1] . "," . $rgb[2] . "," . $opacity . ")";
+    }
+
+    public function hex2rgb($hex) {
+        $hex = str_replace("#", "", $hex);
+
+        if(strlen($hex) == 3) {
+            $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+            $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+            $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+        } else {
+            $r = hexdec(substr($hex,0,2));
+            $g = hexdec(substr($hex,2,2));
+            $b = hexdec(substr($hex,4,2));
+        }
+        $rgb = array($r, $g, $b);
+        //return implode(",", $rgb); // returns the rgb values separated by commas
+        return $rgb; // returns an array with the rgb values
     }
 
     public function pathFunction($_route, $options = array())
